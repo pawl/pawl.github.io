ef4053c95044fe405ef576542b44e82ef986ee09
Paul Brown <paul90brown@gmail.com>
2015-10-06 01:59:35 -0500

    add resume version without border, update pdf resume

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def get_resume_json():
+    with open('resume.json') as data_file:
+        return json.load(data_file)
+
+
 @app.route('/')
 def resume():
-    with open('resume.json') as data_file:
-        data = json.load(data_file)
+    template_data = get_resume_json()
+    return render_template('resume.html', **template_data)
 
-    return render_template('resume.html', **data)
+
+@app.route('/resume_no_border.html')
+def resume_no_border():
+    template_data = get_resume_json()
+    return render_template('resume_no_border.html', **template_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
